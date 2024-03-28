e90f714ad2c45e22307e2c2c62d3d36c6525bdbd
Daria Khanina <khanina_dasha@ukr.net>
2024-03-28 19:56:20 +0200

    commit test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import {  Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import {Layout} from "./Layout";
+import { lazy } from 'react';
 
-// const HomePage = lazy(() => import("../pages/Home"));
-// const RegisterPage = lazy(() => import("../pages/Register"));
+
+const HomePage = lazy(() => import("../pages/Home"));
+const RegisterPage = lazy(() => import("../pages/Register"));
 // const LoginPage = lazy(() => import("../pages/Login"));
 // const ContactsPage = lazy(() => import("../pages/Contacts"));
 
@@ -14,8 +16,8 @@ export default function App() {
       <Suspense fallback={null}>
         <Routes>
           <Route path="/" element={<Layout />} />
-            <Route index element={<div>HOME</div>} />
-            <Route path="/register" element={<div>REGISTER</div>} />
+            <Route index element={<HomePage/>} />
+            <Route path="/register" element={<RegisterPage/>} />
             <Route path="/login" element={<div>LOGIN</div>} />
             <Route path="/contacts" element={<div>CONTACTS</div>} />
         </Routes>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+      <>
+        <h1>Home</h1>
+  
+        <div>
+          <h1>
+            Task manager welcome page{' '}
+            <span role="img" aria-label="Greeting icon">
+              💁‍♀️
+            </span>
+          </h1>
+        </div>
+      </>
+    );
+  }
+  
