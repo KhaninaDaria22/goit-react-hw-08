2bc168491247fb7d7aa3f86bac94e4671230fcd7
Daria Khanina <khanina_dasha@ukr.net>
2024-03-28 20:19:31 +0200

    commit 12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import {  Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import {Layout} from "./Layout";
@@ -6,23 +5,21 @@ import { lazy } from 'react';
 
 
 const HomePage = lazy(() => import("../pages/Home"));
-const RegisterPage = lazy(() => import("../pages/Register"));
+// const RegisterPage = lazy(() => import("../pages/Register"));
 // const LoginPage = lazy(() => import("../pages/Login"));
 // const ContactsPage = lazy(() => import("../pages/Contacts"));
 
 export default function App() {
   return (
-    <Layout>
-      <Suspense fallback={null}>
-        <Routes>
+    <>
+      <Routes>
           <Route path="/" element={<Layout />} />
             <Route index element={<HomePage/>} />
-            <Route path="/register" element={<RegisterPage/>} />
+            {/* <Route path="/register" element={<RegisterPage/>} /> */}
             <Route path="/login" element={<div>LOGIN</div>} />
             <Route path="/contacts" element={<div>CONTACTS</div>} />
         </Routes>
-      </Suspense>
       <Toaster />
-    </Layout>
+    </>     
   );
 }
